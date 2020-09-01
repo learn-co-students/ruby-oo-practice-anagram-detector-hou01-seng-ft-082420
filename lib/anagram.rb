340b91d165c4720e617fd86a7c94db9476769fbf
stephenandersondev <stephenandersondev@gmail.com>
2020-09-01 12:31:33 -0500

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -10,13 +10,11 @@ class Anagram
     def match(array)
         final_array = []
         count = 0
-        new_array = array.collect {|word| (word.split("")).sort}
-        new_array.each do |word|
+        array.collect {|word| (word.split("")).sort}.each{|word|
             if word == @word_array
                 final_array << array[count]
             end
-            count +=1
-        end
+            count +=1}
         final_array
     end
 end
